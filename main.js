4f7790464cde11807477a7c26012b42cc29a9bea
Jessica Williams <jvictoriaw@hotmail.co.uk>
2026-04-18 14:02:12 +0100

    Add hover effect so divs change colour when mouse passes over

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const container = document.querySelector('#container');
 
+// Create the grid of squares
 for (let i = 0; i < 256; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
         container.appendChild(square);
+
+        // Change the background colour of a square when mouse enters
+        square.addEventListener('mouseenter', function() {
+            this.classList.add('hovered');
+        });
+
 }
