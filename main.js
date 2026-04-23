193a0af7bae61a780953cb6049ba75c7b70afcfd
Jessica Williams <jvictoriaw@hotmail.co.uk>
2026-04-23 22:34:55 +0100

    Add button that displays pop up asking user for the number of squares per side for new grid, then removes existing grid and generates new one

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ for (let i = 0; i < 256; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
         container.appendChild(square);
+        square.style.width = (100 / 16) + "%"
 
         // Change the background colour of a square when mouse enters
         square.addEventListener('mouseenter', function() {
@@ -12,3 +13,41 @@ for (let i = 0; i < 256; i++) {
         });
 
 }
+
+const squareAmount = document.getElementById('square-amount')
+squareAmount.addEventListener('click', function() {
+    let size;
+
+    do {
+        const input = prompt('How many squares?');
+        if (input === null) {
+            return;
+        }
+
+        size = Number(input);
+
+        if(isNaN(size)) {
+            alert('please enter a number')
+        } else if (size > 100 || size <= 0) {
+            alert('please enter a number between 1 - 100')
+        }
+     } while (isNaN(size) || size > 100 || size <= 0);
+
+     container.innerHTML = "";
+
+
+     for (let j = 0; j < size * size; j++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
+
+          square.style.width = (100/size) + "%";
+
+        // Change the background colour of a square when mouse enters
+        square.addEventListener('mouseenter', function() {
+            this.classList.add('hovered');
+        });
+
+}
+})
+
